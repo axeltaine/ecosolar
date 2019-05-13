ff781b9703799e61ecd22c5496108fc3579cc0b8
axeltaine <a.taine@simplon-charleville.fr>
2019-05-13 08:51:01 +0200

    carte

--- index.php
+++ index.php
@@ -56,7 +56,11 @@
           </div>
 
           <div class="form-group">
-            <label for="adresse">Adresse*</label> : <input type="text" name="adresse" id="adresse" />
+            <label for="adresse">Adresse*</label> : <iframe
+              src="https://www.google.com/maps/embed?pb=!1m13!1m11!1m3!1d158.0865126246605!2d4.721117603613022!3d49.769271626808745!2m2!1f0!2f45!3m2!1i1024!2i768!4f35!5e1!3m2!1sfr!2sfr!4v1557730017861!5m2!1sfr!2sfr"
+              width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe><input type="text"
+              name="adresse" id="adresse" />
+
           </div>
 
           <input type="button" name="next" class="next action-button" value=" suivant" />
@@ -65,49 +69,48 @@
         <fieldset>
           <h2 class="fs-title">Informations sur la toiture</h2>
           <div class="container">
-          <div class="row">
-          <div class="col">
-          <div class="form-group">
-            <label for="orientation">Orientation*</label> : <br>
-            <!-- ligne contenant l'orientation avec les triangles animer en jquery -->
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 d-flex bloc_orientation">
-              <div class="maison">
-                <img id="ouest" src="img/ouest.png" />
-                <img id="nord_ouest" src="img/nord_ouest.png" />
-                <img id="nord" src="img/nord.png" />
-                <img id="est" src="img/est.png" />
-                <img id="nord_est" src="img/nord-est.png" />
+            <div class="row">
+              <div class="col">
+                <div class="form-group">
+                  <label for="orientation">Orientation*</label> : <br>
+                  <!-- ligne contenant l'orientation avec les triangles animer en jquery -->
+                  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 d-flex bloc_orientation">
+                    <div class="maison">
+                      <img id="ouest" src="img/ouest.png" />
+                      <img id="nord_ouest" src="img/nord_ouest.png" />
+                      <img id="nord" src="img/nord.png" />
+                      <img id="est" src="img/est.png" />
+                      <img id="nord_est" src="img/nord-est.png" />
+                    </div>
+                    <div class="button">
+                      <button id="button_ouest" type="button" class="btn btn-primary">Ouest</button>
+                      <button id="button_nord_ouest" type="button" class="btn btn-primary">Nord_ouest</button>
+                      <button id="button_nord" type="button" class="btn btn-primary">Nord</button>
+                      <button id="button_est" type="button" class="btn btn-primary">Est</button>
+                      <button id="button_nord_est" type="button" class="btn btn-primary">Nord_est</button>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div class="button">
-                <button id="button_ouest" type="button" class="btn btn-primary">Ouest</button>
-                <button id="button_nord_ouest" type="button" class="btn btn-primary">Nord_ouest</button>
-                <button id="button_nord" type="button" class="btn btn-primary">Nord</button>
-                <button id="button_est" type="button" class="btn btn-primary">Est</button>
-                <button id="button_nord_est" type="button" class="btn btn-primary">Nord_est</button>
+              <!-- fin du bloc orientation -->
+              <div class="col">
+                <div id="angle_form">
+
+
+                  
+
+
+                  <div class="form-group">
+                    <label for="customRange2">Angle de votre toiture</label>
+                    <canvas class="moncanvas" id="canvas" width="500" height="500"></canvas>
+                    <input type="range" class="custom-range" min="0" max="50" step="0.1" id="customRange2" value=""
+                      onchange="updateTextInput(this.value);">
+                    <input type="text" id="textInput" value="">
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          </div>
-          <!-- fin du bloc orientation -->
-          <div class="col">
-          <div id="angle_form">
-
-
-            <canvas class="moncanvas" id="canvas" width="500" height="500"></canvas>
-
-
-              <div class="form-group">
-                <label for="customRange2">Angle de votre toiture</label>
-                <input type="range" class="custom-range"
-                min="0" max="50" step="0.1"
-                id="customRange2" value=""
-                onchange="updateTextInput(this.value);"></br>
-                <input type="text" id="textInput" value="">
-              </div>
-          </div>
-          </div>
-          </div>
-  </div>
           <div class="form-group">
             <label for="surface">Surface*</label> : <input type="text" name="surface" id="surface" />
           </div>
@@ -203,7 +206,9 @@
   </script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js">
   </script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+  </script>
   <script src="test.js"></script>
   <script src="maison.js"></script>
   <script src="angle.js"></script>
