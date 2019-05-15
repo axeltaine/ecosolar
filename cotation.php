9a7657c20441e79eab411ff3add76a18bd21eaca
axeltaine <a.taine@simplon-charleville.fr>
2019-05-15 15:39:15 +0200

    cotation

--- cotation.php
+++ cotation.php
@@ -1,37 +1,44 @@
-<div class="container-fluid">
-    <div class="col-sm">
+<div class="container">
+    <div class="row">
     <img src="cotation.png" class="img-thumbnail" style="height:8em; width:8em;">
-        <div id="dimensions-inputs-container" class="col-xs-12 col-sm-6 col-md-12">
-            <div class="form-group string optional study_roof_length col-xs-12 col-md-6 col-lg-4"><label
+</div>
+</div>
+
+
+            <div class="form-group string optional study_roof_length "><label
                     class="string optional small" for="study_roof_length">Longeur</label>
-                <div class="">
+                
                     <div class="input-group">
                         <input class="numeric float optional form-control" min="0" placeholder="0" type="number"
                             step="any" value="10" name="study[roof_length]" id="study_roof_length">
-                        <span class="input-group-addon">m</span>
-                    </div>
+                            <div class="input-group-append">
+    <label class="input-group-text" for="inputGroupSelect02">m</label>
+  </div>
+                    
                 </div>
             </div>
-            <div class="form-group string optional study_roof_width col-xs-12 col-md-6 col-lg-4"><label
+            <div class="form-group string optional study_roof_width "><label
                     class="string optional small" for="study_roof_width">Largeur</label>
-                <div class="">
+                
                     <div class="input-group">
                         <input class="numeric float optional form-control" min="0" placeholder="0" type="number"
                             step="any" value="10" name="study[roof_width]" id="study_roof_width">
-                        <span class="input-group-addon">m</span>
-                    </div>
+                         <div class="input-group-append">
+    <label class="input-group-text" for="inputGroupSelect02">m</label>
+  </div>
+                    
                 </div>
             </div>
-            <div class="form-group string optional study_roof_height col-xs-12 col-md-6 col-lg-4"><label
+            <div class="form-group string optional study_roof_height "><label
                     class="string optional small" for="study_roof_height">Hauteur</label>
-                <div class="">
+                
                     <div class="input-group">
                         <input class="numeric float optional form-control" min="0" placeholder="0" type="number"
                             step="any" value="3" name="study[roof_height]" id="study_roof_height">
-                        <span class="input-group-addon">m</span>
-                    </div>
+                         <div class="input-group-append">
+    <label class="input-group-text" for="inputGroupSelect02">m</label>
+  </div>
+                    
                 </div>
             </div>
-        </div>
-    </div>
-</div>
+        
